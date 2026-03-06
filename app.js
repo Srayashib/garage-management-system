f488b3d3fae231124807bec5897b4786a7e63b57
Srayashib <srayashibasu@gmail.com>
2026-03-06 23:50:49 +0530

    port fix

--- app.js
+++ app.js
@@ -69,6 +69,8 @@ app.use(pageRoutes);
 app.use(express.urlencoded({ extended: true }));
 app.use(eventRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running on port", process.env.PORT);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
